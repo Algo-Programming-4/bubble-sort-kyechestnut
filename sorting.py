1230f37801dfcd580aa053f5323f107fe249949c
kyechestnut <chestkye000@alpinesd.org>
2024-10-02 12:28:29 -0600

    sorting.py

--- sorting.py
+++ sorting.py
@@ -1,20 +1,17 @@
-lis = [10000, 5, 8, 11, 12, 5, 12, 18, 200, 1, 7]
 
-def bubble (lis, length):
+def bubble (lis):
+    length = len(lis)
     while length > 0:
         itterations =0
-        x = 0
+        x = 1
         while x < length:
-            if lis[x] > lis[x+1]:
+            if lis[x] < lis[x-1]:
                 nw = lis[x]
-                lis[x] = lis[x+1]
-                lis[x+1] = nw
+                lis[x] = lis[x-1]
+                lis[x-1] = nw
             x+=1
             itterations +=1
         length-=1
     print(x)
     return lis
 
-
-aList = bubble(lis, len(lis)-1)
-print ("Final:", aList)
